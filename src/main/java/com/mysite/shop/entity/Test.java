411eb9e19ed39b67ac617a596654a445c0a09071
KU <vebvdev@gmail.com>
2023-08-07 18:24:04 +0900

    web shopping ex first commit

---
+++ src/main/java/com/mysite/shop/entity/Test.java
@@ -0,0 +1,26 @@
+package com.mysite.shop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Getter @Setter
+@ToString
+public class Test {
+    @Id
+    @Column(name="Test_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String address;
+}
